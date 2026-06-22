4cdacb8d91d5fe50decd5798d549afc04a904afd
sahilslomo <sahilslomo@gmail.com>
2026-06-22 19:40:28 +0530

    2nd

--- src/app/lib/firebase.ts
+++ src/app/lib/firebase.ts
@@ -1,19 +1,18 @@
 import { initializeApp } from "firebase/app";
 import {
-  getAuth,
-  setPersistence,
-  browserLocalPersistence,
+    getAuth,
+    setPersistence,
+    browserLocalPersistence,
 } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyD-CQ6PEYCUip5McM5lPp07GK7vOGaamIc",
-  authDomain: "navik-c4443.firebaseapp.com",
-  databaseURL: "https://navik-c4443-default-rtdb.firebaseio.com",
-  projectId: "navik-c4443",
-  storageBucket: "navik-c4443.firebasestorage.app",
-  messagingSenderId: "394338096150",
-  appId: "1:394338096150:web:2482d814ce430a7d371137",
-  measurementId: "G-006YE7FHJZ",
+    apiKey: "AIzaSyBXVFxfT12IXSJHzGAhKpQF18sPmBOMutQ",
+    authDomain: "navik-reloaded.firebaseapp.com",
+    projectId: "navik-reloaded",
+    storageBucket: "navik-reloaded.firebasestorage.app",
+    messagingSenderId: "557866754279",
+    appId: "1:557866754279:web:491ed0279ca0fc3c428242",
+    measurementId: "G-0KJWKY67WE"
 };
 
 const app = initializeApp(firebaseConfig);
@@ -22,9 +21,9 @@ export const auth = getAuth(app);
 
 // 🔥 KEEP USER LOGGED IN AFTER REFRESH
 setPersistence(auth, browserLocalPersistence)
-  .then(() => {
-    console.log("Firebase persistence enabled");
-  })
-  .catch((err) => {
-    console.error("Persistence error:", err);
-  });
+    .then(() => {
+        console.log("Firebase persistence enabled");
+    })
+    .catch((err) => {
+        console.error("Persistence error:", err);
+    });
